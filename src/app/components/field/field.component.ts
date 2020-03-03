4479f4b6e8f314209fdf1bea24c0b7f72aed9a67
Anton Breidak <abreydak@bk.ru>
2020-03-03 16:08:58 +0300

    shuffling mechanism, markup

--- src/app/components/field/field.component.ts
+++ src/app/components/field/field.component.ts
@@ -24,72 +24,86 @@ export class FieldComponent implements OnChanges {
     this.columnsProperty = {
       'grid-template-columns': `repeat(${this.fieldSize}, 1fr)`
     }
-    console.log(this.shuffleSteps, 'shuffle steps on changes')
-    this.shuffle()
+    if (this.shuffleSteps) this.shuffle()
     this.gameMessage.emit('Good luck!')
   }
 
   shuffle() {
-    console.log('Shuffling with', this.shuffleSteps, 'steps')
-    if (this.shuffleSteps) {
-      let stepsTaken = 0
-      let currentTileId = this.generateNewTileId()
-      //while (stepsTaken < this.shuffleSteps) {
-      for (let i=0;i<15;i++) {
-        let shifted = this.tryToShift(currentTileId)
-        console.log('shifted', shifted)
-        if (shifted) stepsTaken++
-        currentTileId = this.generateNewTileId(currentTileId)
-      }
-      console.log('stepsTaken', stepsTaken)
-    }    
-  }
-
-  private generateNewTileId(prevId: number = -1): number {
-    let maxTileId = this.tiles.length - 1
-    let newTileId = getRandomInt(0, maxTileId)
-    let idsAreTheSame = newTileId == prevId
-    console.warn('ID OVERLAP', idsAreTheSame)
-    while (idsAreTheSame) {
-      newTileId = getRandomInt(0, maxTileId)
-      idsAreTheSame = newTileId == prevId
+    let prevShuffledTile = 0;
+    let stepsTaken = 0
+    while (stepsTaken < this.shuffleSteps) {
+      let tileId = this.buildShiftableTileId(prevShuffledTile)
+      this.shift(tileId)
+      prevShuffledTile = tileId
+      stepsTaken++
     }
-    return newTileId
-  }
-
-  private tryToShift(fromId: number): boolean {
-    let shiftCases = [1, -1, this.fieldSize, -this.fieldSize]
-    let fromPos = this.tiles.indexOf(fromId)
-    let zeroPos = this.tiles.indexOf(0)
-    let diff = fromPos - zeroPos;
-    // let debugObj = {fromPos, zeroPos, diff}
-    // console.table(debugObj)
-    if (shiftCases.includes(diff)) {
-      this.tiles[fromPos] = 0
-      this.tiles[zeroPos] = fromId
-      return true
-    } return false
   }
 
   handleTileClick(e) {
     let tileId = parseInt(e.target.innerText)
     if (tileId) {
-      let shifted = this.tryToShift(tileId)
-      let won = this.hasWon()
+      let won = false
+      let shifting = this.isTileSuitable(tileId)
+      if (shifting) {
+        this.shift(tileId)
+        won = this.hasWon()
+      }
       if (won) {
         this.turns++
         this.gameMessage.emit(`Victory! Turns taken: ${this.turns}`)
-      } else if (shifted) {
+      } else if (shifting) {
         this.turns++
         this.gameMessage.emit(`Turns taken: ${this.turns}`)
       } else this.gameMessage.emit(`Impossible turn! Total: ${this.turns}`)
     }
   } 
 
+
+  private buildShiftableTileId(prevId: number): number {
+    let suitableTiles = this.tiles.filter(tileId => this.isTileSuitable(tileId, prevId))
+    let randomlyChosenTileId = suitableTiles[Math.floor(Math.random() * suitableTiles.length)]
+    console.log('Tile chosen when shuffling', randomlyChosenTileId)
+    return randomlyChosenTileId
+  }
+
+  private isTileSuitable(tileId: number, prevId?: number): boolean {
+    // Tile shiftability conditions:
+    // 1. Haven't shifted in the last turn
+    // 2. Is adjacent to empty tile
+    // 3. If shifting horizontally, only the same row turns are allowed
+    let tilePos = this.tiles.indexOf(tileId)
+    let result = true
+    if (prevId) result = tileId !== prevId
+    if (result) {
+      let zeroPos = this.tiles.indexOf(0)
+      let suitabilityCases = [1, -1, this.fieldSize, -this.fieldSize]
+      let adjacency = suitabilityCases.findIndex(x => tilePos + x == zeroPos)
+      result = adjacency > -1
+      if (result) {
+        let isAdjacentHorizontally = [0, 1].includes(adjacency)
+        if (isAdjacentHorizontally) result = this.checkSameRowCondition(tilePos, zeroPos)
+      }
+    }
+    return result
+  }
+
+  private checkSameRowCondition(tilePos: number, zeroPos: number): boolean {
+    let tilePosRow = Math.floor(tilePos /  this.fieldSize)
+    let zeroPosRow = Math.floor(zeroPos /  this.fieldSize)
+    return tilePosRow == zeroPosRow
+  }
+
+  private shift(tileId: number): void {
+    let tilePos = this.tiles.indexOf(tileId)
+    let zeroPos = this.tiles.indexOf(0)
+    this.tiles[tilePos] = 0
+    this.tiles[zeroPos] = tileId
+  }
+
   private hasWon(): boolean {
     let zeroPos = this.tiles.indexOf(0)
     if (zeroPos == this.tiles.length - 1) {
-      console.log('Checking if won')
+      // We only check winning condition if the last tile is empty
       let tileArray = this.buildTileArray()
       return tileArray.every((x, i) => x == this.tiles[i])
     } return false    
